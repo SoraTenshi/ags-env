9b6ff7073ee60afb4cb2d3ae900bc52fa5f45506
SoraTenshi <dream@neoncity.dev>
2023-11-26 02:41:11 +0100

    Tooltip

--- bar/modules/weather.js
+++ bar/modules/weather.js
@@ -13,6 +13,7 @@ const options = {
 
 export const Weather = () => Widget.Box({
   class_name: 'weather',
+  has_tooltip: true,
   children: [Widget.Label({
       class_name: 'weather-icon',
       css: "font-family: 'Material Symbols Sharp'",
@@ -23,15 +24,23 @@ export const Weather = () => Widget.Box({
   connections: [[thirtyMinutes, self => {
     Utils.fetch('http://wttr.in/?format=j1', options)
       .then(res => {
-        const weather = JSON.parse(res);
+        let weather = JSON.parse(res);
+        let weatherCondition = weather['current_condition'][0];
 
         /** @type {string} */
-        const icon = Icon.weather[weather['current_condition'][0]['weatherCode']];
+        const icon = Icon.weather[weatherCondition['weatherCode']];
 
         /** @type {string} */
-        const temp = weather['current_condition'][0]['temp_C'];
+        const temp = weatherCondition['temp_C'];
         self.children[0]['label'] = icon;
         self.children[1]['label'] = ` ${temp.replaceAll('+', '')}°C`;
+
+        const location = weather['nearest_area'][0];
+        const city = location['areaName'][0]['value'];
+        const country = location['country'][0]['value'];
+        self.tooltip_markup = `Location: ${city}, ${country}` + '\n' +
+          `FeelsLike: ${weatherCondition['FeelsLikeC'].replaceAll('+', '')}°C` + '\n' +
+          `Humidity: ${weatherCondition['humidity']}%`;
       })
       .catch(err => console.error(err));
   }]]
