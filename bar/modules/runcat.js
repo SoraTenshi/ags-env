8d239a1d5851942a57a6ee2ae19ab3569c358177
SoraTenshi <dream@neoncity.dev>
2024-05-08 23:21:16 +0200

    Runcat types jsdoc

--- bar/modules/runcat.js
+++ bar/modules/runcat.js
@@ -1,32 +1,35 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
 import * as Utils from "resource:///com/github/Aylur/ags/utils.js";
-import { Variable } from "resource:///com/github/Aylur/ags/variable.js";
 
-const cycle = ['0', '1', '2', '3', '4'];
+/** @type{["0", "1", "2", "3", "4"]} */
+const cycle = ["0", "1", "2", "3", "4"];
 let current_cycle = 4;
+let last_cpu = 1;
 
-const divide = ([total, free]) => free / total;
-const cpu = new Variable(1, {
-    // @ts-ignore
-    poll: [10, 'top -b -n 1', out => divide([100, out.split('\n')
-        .find(line => line.includes('Cpu(s)'))
-        .split(/\s+/)[1]
-        .replace(',', '.')])],
-})
-
-const as_cycle = (r) => {
+/**
+  @type {(r: string) => "sleep" | "0" | "1" | "2" | "3" | "4"}
+  */
+const as_cycle = (r) => { 
   const curr = Math.round(Number(r));
-  let selected = 'sleep';
-  if(curr > 0) selected = cycle[cycle.length % current_cycle];
+  /** @type{"sleep" | "0" | "1" | "2" | "3" | "4"}*/
+  let selected = "sleep";
 
+  if(curr > 0) selected = cycle[cycle.length % current_cycle];
+  
+  last_cpu = curr;
   return selected;
 };
 
 export const RunCat = () => Widget.Box({
   setup: self => {
-      self.tooltip_markup = self.children[0].shown.bind || "sleep";
-  },
+      const stack = self.children[0];
+      self.poll(1000 / last_cpu, () => {
+          Utils.execAsync(["sh", "-c", `top -bn1 | grep '%Cpu' | tail -1 | awk '{print 100-$8}'`])
+            .then((r) => stack.shown = as_cycle(r))
+            .catch((err) => print(err));
+      });
+    },
 
   children: [
     Widget.Stack({
@@ -38,8 +41,7 @@ export const RunCat = () => Widget.Box({
         '4': Widget.Icon({icon: "cat_4-symbolic"}),
         'sleep': Widget.Icon({icon: "cat_sleep-symbolic"}),
       },
-      // @ts-ignore
-      shown: cpu.bind().as(r => as_cycle(r)),
+      shown: 'sleep',
     })
   ]
 });
