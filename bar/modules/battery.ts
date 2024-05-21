27407582b852fa03f64880b2d126289ae43741f0
SoraTenshi <dream@neoncity.dev>
2024-05-21 13:08:47 +0200

    Update battery.ts

--- bar/modules/battery.ts
+++ bar/modules/battery.ts
@@ -2,10 +2,32 @@ import { Icon } from 'widgets/icons';
 
 const battery = await Service.import('battery');
 
+const tooltip = Variable<string>("");
+
+const format_seconds = (seconds: number): string => {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+
+  const fmt_hrs = hrs.toString().padStart(2, '0');
+  const fmt_mins = mins.toString().padStart(2, '0');
+  const fmt_secs = secs.toString().padStart(2, '0');
+
+  return `${fmt_hrs}h:${fmt_mins}m:${fmt_secs}s`;
+}
+
 export const Battery = () => Widget.Box({
+  setup: () => {
+    Utils.interval(5000, () => {
+      const percent = battery.percent;
+      const charging = battery.charging;
+      const time_left = battery.time_remaining;
+      tooltip.value = `Current: ${percent}%\nIs Charging: ${charging ? 'yes' : 'no'}\nTime left: ${format_seconds(time_left)}`;
+    });
+  },
   class_name: 'battery',
   visible: battery.bind('available'),
-  tooltip_markup: battery.bind('percent').as(p => `Current: ${p}%`),
+  tooltip_markup: tooltip.bind(),
   children: [
     Widget.Label({
       class_name: 'battery-icon',
